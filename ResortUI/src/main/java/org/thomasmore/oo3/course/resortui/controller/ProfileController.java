827910987cb6bcd36f3f22ac9bcc889b32a6f91c
oliviervdp <r0478364@student.thomasmore.be>
2014-11-20 11:33:44 +0100

    Test commit

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/ProfileController.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/ProfileController.java
@@ -16,10 +16,27 @@
  */
 package org.thomasmore.oo3.course.resortui.controller;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.thomasmore.oo3.course.resortui.model.ProfilePageDto;
+
 /**
  *
  * @author olivier
  */
+
+@Named(value="profile")
+@RequestScoped
 public class ProfileController {
+    
+    private ProfilePageDto dto = new ProfilePageDto();
+
+    public ProfilePageDto getDto() {
+        return dto;
+    }
+
+    public void setDto(ProfilePageDto dto) {
+        this.dto = dto;
+    }
     
 }
